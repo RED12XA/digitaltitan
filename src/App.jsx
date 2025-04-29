7f9339b21bbec011a8768569d3dd067e30aaa77f
red12xa <ridaessbayi@gmail.com>
2025-04-29 18:13:29 +0200

    router dom push

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 
 import './App.css'
 import Home from './pages/home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
