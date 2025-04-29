358e0534f0d16de1973b8f4624b948602b5e8ee1
red12xa <ridaessbayi@gmail.com>
2025-04-29 20:15:05 +0200

    add bootstrap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Use Link if using React Router
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <a href="/" className="navbar-logo">
-          YOUR LOGO
-        </a>
-        <ul className="nav-menu">
-          <li className="nav-item">
-            <a href="/" className="nav-link">Home</a>
-          </li>
-          <li className="nav-item">
-            <a href="/about" className="nav-link">About</a>
-          </li>
-          <li className="nav-item">
-            <a href="/services" className="nav-link">Services</a>
-          </li>
-          <li className="nav-item">
-            <a href="/contact" className="nav-link">Contact</a>
-          </li>
-        </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="/">TiTans<span>Blog</span></a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Home</a>
+              {/* Use <Link to="/">Home</Link> if using React Router */}
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/services">Services</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
