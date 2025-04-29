59272babf12208ef949acbfe54f2a64683f9cf74
red12xa <ridaessbayi@gmail.com>
2025-04-29 18:38:00 +0200

    fix issues done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-
-import './App.css'
-import Home from './pages/home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import About from './pages/About';
-import NotFound from './pages/NotFound';
+import './App.css';
 
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+import Destinations from './pages/Destinations';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <>
-      <Home/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/destinations" element={<Destinations />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/postguest.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Postguest() {
+  return (
+    <div>postguest</div>
+  )
+}
+
+export default Postguest
